cc462531be168ff5267c8ab8de5e9fef3c186da4
lauriciodev <developerwebmasterpro@gmail.com>
2023-04-07 18:43:43 -0300

    upload de fotos parte 2

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -2,25 +2,19 @@ import { extname, resolve } from "path";
 import multer from "multer";
 
 const destino = resolve(__dirname, "..", "..", "uploads", "images");
+const aleatorio = () => Math.floor(Math.random() * 12000 + 20002);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
+      return cb(new multer.MulterError("Formato deve ser jpeg ou png."));
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: destino,
-    /* filename: (req, file, cb) => {
-      cb(
-        null,
-        `${Date.now()}_${file.originalname}${extname(file.originalname)} `
-      );
-    }, */
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${aleatorio}${extname(file.originalname)}`);
+    },
   }),
 };
-
-/*
-fileFilter: (req, file, cb) => {
-  if (file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
-    return cb(new multer.MulterError("Formato deve ser jpeg ou png."));
-  }
-  return cb(null, true);
-}, */
-
-/* const aleatorio = () => Math.floor(Math.random() * 12000 + 20002); */
