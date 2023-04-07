341fc20dc2f7e43fc46efa99e1ee3641bf27c1d1
lauriciodev <developerwebmasterpro@gmail.com>
2023-04-07 11:06:13 -0300

    multer upload de arquivos parte 2

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,12 @@ import multer from "multer";
 const aleatorio = () => Math.floor(Math.random() * 12000 + 20002);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
+      return cb(new multer.MulterError("Formato deve ser jpeg ou png."));
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, "..", "..", "uploads"));
--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,8 +1,18 @@
-// import Aluno from "../models/aluno";
+import multer from "multer";
+import multerConfig from "../config/multerConfig";
+
+const upload = multer(multerConfig).single("foto");
 
 class FotoContronller {
   async store(req, res) {
-    res.json(req.file);
+    upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
